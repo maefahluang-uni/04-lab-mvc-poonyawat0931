60d975305facc7b039771d3599d6fea1cc4b4ead
poonyawat0931 <6531503055@lamduan.mfu.ac.th>
2023-09-11 16:43:22 +0700

    finish1

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,22 +5,53 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
+    private int id ; 
+    private String title ; 
+    private String performer ; 
+    private Date date; 
+    private String description ; 
+
 
     public Concert() {
     }
     public Concert(String title, String description) {
         //TODO: set attributes
+        this.title = title;
+        this.description = description ; 
+        
     }
- 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     public String getTitle() {
-        //TODO: return title
-        return "";
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getPerformer() {
+        return performer;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
     }
     public String getDescription() {
-        //TODO: return description
-        return "";
+        return description;
     }
-
-    //TODO: add getters and setters
+    public void setDescription(String description) {
+        this.description = description;
+    }
+ 
+   
+    
 
 }
